226adf6f987b0a81aa9c8dfa27caedffa45981b1
Solomon <solomonyu1@gmail.com>
2020-02-14 23:35:51 -0800

    updated stuff

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
- 
-var app = require('express')();
+var express = require('express'); 
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 var portnum = 8080;
 var currentUsers = 0;
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('public'));
+
+// app.get('/', function(req, res){
+//     res.sendFile(__dirname + '/public/index.html');
+// });
 
 io.on('connection', function(socket){
     console.log("new user connected");
