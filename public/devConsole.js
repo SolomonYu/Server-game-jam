dcb5eefbea6a64c5a5e7164a297442073dca1fab
Cameron <csavage5@me.com>
2020-02-15 16:01:00 -0800

    removed functions from devConsole

--- public/devConsole.js
+++ public/devConsole.js
@@ -7,19 +7,11 @@ var vote = {
     "op4" : "four"
 }
 
+votes = [0,0,0,0]
+
 function sendRequestVote(){
     console.log("Sending vote request to server...")
     io.emit("requestVote", vote)
-}
-
-function sendSendResults(){
-    console.log("Sending following vote results to game client:")
-    console.log(`Option 1: ${votes.op1} votes`)
-    console.log(`Option 2: ${votes.op2} votes`)
-    console.log(`Option 3: ${votes.op3} votes`)
-    console.log(`Option 4: ${votes.op4} votes`)
-    io.emit("sendResults", votes)
-    console.log("Sending vote totals...")
 }
 
 // socket.on('displayVotes', function(vote) {
